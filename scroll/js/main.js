2d45e3bd276392d725ee5c3e0b8c5385d906056b
LucianoNascimento1982 <47835610+LucianoNascimento1982@users.noreply.github.com>
2019-03-12 14:53:25 -0300

    Add files via upload
    diretorio scroll ok

---
+++ scroll/js/main.js
@@ -0,0 +1,11 @@
+//$('.navbar a').on('clic',function(e){
+ //if( this.hash !='');{
+    // e.preventDefault();
+
+    // const hash=this .hash;
+    // $('html,body').animate({
+    //scrolltop:$(hash).offset().top
+    // },800
+    // );
+ //}
+// });
